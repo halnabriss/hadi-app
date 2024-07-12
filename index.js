b0319c436a90204c34f67502ce49f0d97977a4a0
hadi alnabriss <alnabris@gmail.com>
2024-07-12 14:15:33 +0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = 5050;
 const app =express();
 
-app.get('/' , (req,res) => res.send('<h1>Hello .....Hadi..A..V2...With Vol2222</h1>'));
-app.get('/help' , (req,res) => res.send('<h1>How ...A.can I help you?..With Vol222</h1>'));
+app.get('/' , (req,res) => res.send('<h1>Hello...</h1>'));
+app.get('/help' , (req,res) => res.send('<h1>How. Can I Help You?</h1>'));
 
-app.listen(PORT, () => console.log('app .....is upA and running....V2...With Vol2222'));
+app.listen(PORT, () => console.log('app is running.....'));
