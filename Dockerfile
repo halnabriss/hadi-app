928e8d5af74e686812df829aabb09d0cdbb0bdb9
hadi alnabriss <alnabris@gmail.com>
2024-07-12 14:00:31 +0300

    new update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:14
 WORKDIR /hadiapp
-COPY . .
+COPY package.json .
 RUN npm install
+COPY . .
 EXPOSE 5050
-CMD [ "npm", "start" ]
+CMD [ "npm", "run" , "start-dev" ]
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = 5050;
 const app =express();
 
-app.get('/' , (req,res) => res.send('<h1>Hello Hadi....V2</h1>'));
-app.get('/help' , (req,res) => res.send('<h1>How can I help you?</h1>'));
+app.get('/' , (req,res) => res.send('<h1>Hello .....Hadi..A..V2...With Vol2222</h1>'));
+app.get('/help' , (req,res) => res.send('<h1>How ...A.can I help you?..With Vol222</h1>'));
 
-app.listen(PORT, () => console.log('app is up and running....V2'));
+app.listen(PORT, () => console.log('app .....is upA and running....V2...With Vol2222'));
