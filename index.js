26bb98389aed17655572bef6082814628c57d625
Hadi Alnabriss <54836977+halnabriss@users.noreply.github.com>
2024-07-15 00:21:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = 5050;
 const app =express();
 
-app.get('/' , (req,res) => res.send('<h1>Hello...Fun2</h1>'));
+app.get('/' , (req,res) => res.send('<h1>Hello...Fun3</h1>'));
 app.get('/help' , (req,res) => res.send('<h1>How. Can I Help You?</h1>'));
 
 app.listen(PORT, () => console.log('app is running.....'));
